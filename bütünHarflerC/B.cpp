1461313b24d6e9c375387e343cba3d383372e569
Kutay Kurt <kurtkutay041@gmail.com>
2023-11-09 21:50:00 +0300

    AB

---
+++ bütünHarflerC/B.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int i,j,k,l;
+	
+	for(i=0;i<=8;i++){
+		for(j=0;j<=8;j++){
+			if(i==0&&j==8||i==4&&j==8||i==8&&j==8){
+				printf(" ");
+			}
+			else if(i==0||i==8||i==4||j==0||j==8){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
